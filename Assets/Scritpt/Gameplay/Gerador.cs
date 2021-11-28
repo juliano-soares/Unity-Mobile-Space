450625f522b9fb37a9ab8ebe07360c152d36743c
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2021-11-28 16:48:02 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scritpt/Gameplay/Gerador.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Gerador : MonoBehaviour
+{
+    [SerializeField]
+    private Transform alvo;
+    [SerializeField]
+    private Pontuacao pontuacao;
+
+    [SerializeField]
+    private float tempo;
+    [SerializeField]
+    private Rect area;
+
+    [SerializeField]
+    private ReservaDeInimigos reservaDeInimigos;
+
+    private void Start()
+    {
+        InvokeRepeating("Instanciar", 0f, this.tempo);
+    }
+
+    private void Instanciar()
+    {
+        if (this.reservaDeInimigos.TemInimigo())
+        {
+            var inimigo = this.reservaDeInimigos.PegarInimigo();
+            this.DefinirPosicaoInimigo(inimigo);
+            inimigo.GetComponent<Seguir>().SetAlvo(this.alvo);
+            inimigo.GetComponent<Pontuavel>().SetPontuacao(this.pontuacao);
+        }
+    }
+
+    private void DefinirPosicaoInimigo(GameObject inimigo)
+    {
+        var posicaoAleatoria = new Vector2(
+                Random.Range(this.area.x , this.area.x + this.area.width),
+                Random.Range(this.area.y, this.area.y + this.area.height)
+            );
+        var posicaoInimigo = (Vector2)this.transform.position + posicaoAleatoria;
+        inimigo.transform.position = posicaoInimigo;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(100, 0, 100);
+        var posicao = this.area.position + (Vector2)this.transform.position + this.area.size/2;
+        Gizmos.DrawWireCube(posicao, this.area.size);
+    }
+
+}
---
+++ Assets/Scritpt/UI/NovaPontuacao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NovaPontuacao : MonoBehaviour {
+    [SerializeField]
+    private TextoDinamico textoPontuacao;
+    [SerializeField]
+    private TextoDinamico textoNome;
+    [SerializeField]
+    private Ranking ranking;
+
+    private string id;
+    private Pontuacao pontuacao;
+	
+	private void Start ()
+    {
+        int totalDePontos = this.GetPontuacao();
+        string nomeDaPessoa = this.GetNome();
+        this.textoPontuacao.AtualizarTexto(totalDePontos);
+        this.textoNome.AtualizarTexto(nomeDaPessoa);
+        this.id = this.ranking.AdicionarPontuacao(totalDePontos, nomeDaPessoa);
+    }
+
+    private string GetNome()
+    {
+        if (PlayerPrefs.HasKey("UltimoNome"))
+        {
+            return PlayerPrefs.GetString("UltimoNome");
+        }
+        return "Nome";
+    }
+    private int GetPontuacao()
+    {
+        this.pontuacao = GameObject.FindObjectOfType<Pontuacao>();
+        var totalDePontos = -1;
+        if (this.pontuacao != null)
+        {
+            totalDePontos = this.pontuacao.Pontos;
+        }
+
+        return totalDePontos;
+    }
+
+    public void AlterarNome(string nome)
+    {
+        this.ranking.AlterarNome(nome, this.id);
+        PlayerPrefs.SetString("UltimoNome", nome);
+    }
+}
